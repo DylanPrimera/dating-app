76eb663104f6f27e8a1f719487d81b8506a1686c
DylanPrimera <dylanprimera98@gmail.com>
2025-01-27 19:37:03 -0500

    presence avatar green circle

--- src/components/custom/PresenceAvatar.tsx
+++ src/components/custom/PresenceAvatar.tsx
@@ -3,7 +3,7 @@
 import { usePresenceStore } from "@/hooks";
 import { Badge } from "@heroui/badge";
 import { Avatar, AvatarImage } from "../ui/avatar";
-
+import { cn } from "@/lib";
 
 interface Props {
   userId?: string;
@@ -13,13 +13,18 @@ interface Props {
 export const PresenceAvatar: React.FC<Props> = ({ userId, src }) => {
   const { membersId } = usePresenceStore();
   const isOnline = userId && membersId.indexOf(userId) !== -1;
+
+  console.log(isOnline);
   return (
-    <Badge color="success" className="bg-green-500 border-none" content="" isInvisible={!isOnline}>
+    <Badge
+      color="success"
+      className={cn("bg-green-500 border-none", {
+        hidden: !isOnline,
+      })}
+      content=""
+    >
       <Avatar>
-        <AvatarImage
-          alt="Image of member"
-          src={src || ''}
-        />
+        <AvatarImage alt="Image of member" src={src || ""} />
       </Avatar>
     </Badge>
   );
