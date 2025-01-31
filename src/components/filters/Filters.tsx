af8295a7d2df981c07d1a6cdad94977c619fe2b3
DylanPrimera <dylanprimera98@gmail.com>
2025-01-30 20:17:56 -0500

    use memo for filters, set filters and order by filter with cleear

--- src/components/filters/Filters.tsx
+++ src/components/filters/Filters.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import { FaFemale, FaMale } from "react-icons/fa";
 import { Button } from "../ui/button";
 import { Switch } from "../ui/switch";
@@ -13,9 +13,11 @@ import {
 } from "../ui/select";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { DualRangeSlider } from "../ui/dual-range-slider";
+import { X } from "lucide-react";
 
 export const Filters = () => {
   const [ageValues, setAgeValues] = useState([18, 45]);
+  const [orderValue, setOrderValue] = useState("");
   const orderByList = [
     { label: "Last active", value: "updated" },
     { label: "Newest members", value: "created" },
@@ -35,14 +37,43 @@ export const Filters = () => {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const router = useRouter();
+  
+
   const handleAgeSelect = (values: number[]) => {
-    setAgeValues(values)
     const params = new URLSearchParams(searchParams);
-    params.set('ageRange', values.toLocaleString());
+    setAgeValues(values);
+    params.set("ageRange", values.toLocaleString());
     router.replace(`${pathname}?${params}`);
-   
-    console.log(values);
   };
+
+  const handleOrderBy = (value: string) => {
+    const params = new URLSearchParams(searchParams);
+    setOrderValue(value);
+    params.set("orderBy", value);
+    router.replace(`${pathname}?${params}`);
+  };
+
+  const handleClear = (e: React.MouseEvent) => {
+    const params = new URLSearchParams(searchParams);
+    e.stopPropagation();
+    setOrderValue("");
+    params.delete("orderBy");
+    router.replace(`${pathname}?${params}`);
+  };
+
+  useMemo(() => {
+    const params = new URLSearchParams(searchParams);
+    if(params.get('ageRange') !== undefined) {
+      const ageValues = params.get('ageRange')?.split(',').map((n) => parseInt(n));
+      setAgeValues(ageValues!);
+    } 
+
+    if(params.get('orderBy') !== undefined) {
+      const orderByValue = params.get('orderBy');
+      setOrderValue(orderByValue || '');
+    }
+
+  }, [searchParams]);
 
   return (
     <div className="shadow-md py-3 w-full">
@@ -59,10 +90,14 @@ export const Filters = () => {
           ))}
         </div>
         <div className="flex flex-col itmes-center gap-2 w-1/4">
-            <div className="w-full flex items-center justify-between">
-            <label htmlFor="age-slider" className="text-black">Age range</label>
-            <span className="text-black">{ageValues[0]}-{ageValues[1]}</span>
-            </div>
+          <div className="w-full flex items-center justify-between">
+            <label htmlFor="age-slider" className="text-black">
+              Age range
+            </label>
+            <span className="text-black">
+              {ageValues[0]}-{ageValues[1]}
+            </span>
+          </div>
           <DualRangeSlider
             id="age-slider"
             className="text-black"
@@ -78,11 +113,21 @@ export const Filters = () => {
           <p className="text-sm text-black">With photo</p>
           <Switch defaultChecked color="default" />
         </div>
-        <div className="w-auto">
-          <Select>
+        <div className="relative">
+          <Select value={orderValue} onValueChange={handleOrderBy}>
             <SelectTrigger className="w-[280px]">
-              <SelectValue placeholder="Order by" />
+              <SelectValue placeholder="Select an option" />
             </SelectTrigger>
+            {orderValue && (
+              <Button
+                onClick={handleClear}
+                variant="ghost"
+                className="absolute top-0 right-8 hover:bg-gray-100 p-1 rounded-full hover:bg-transparent"
+              >
+                <X className="h-4 w-4 text-gray-500" />
+              </Button>
+            )}
+
             <SelectContent>
               {orderByList.map((item, index) => (
                 <SelectItem key={index} value={item.value}>
